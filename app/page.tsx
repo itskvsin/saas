ca9e5f911295926cdf0ddd37c248202424fd6061
Kevin Solanki <142715013+itskvsin@users.noreply.github.com>
2026-02-06 04:12:34 +0000

    parallex added

--- app/page.tsx
+++ app/page.tsx
@@ -2,44 +2,41 @@
 
 import { useRef } from "react";
 import Temp from "./components/temp/temp";
+import Animation from "./components/Animations/parallex";
 
 export default function Home() {
-  const heroRef = useRef<HTMLDivElement>(null);
-
-  const handleMouseMove = (e: React.MouseEvent) => {
-    if (!heroRef.current) return;
-
-    const rect = heroRef.current.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-
-    heroRef.current.style.setProperty("--x", `${x}px`);
-    heroRef.current.style.setProperty("--y", `${y}px`);
-  };
 
   return (
     <section>
-      
-    <main
-      ref={heroRef}
-      onMouseMove={handleMouseMove}
-      className="relative flex h-screen w-screen items-center justify-center bg-black"
-    >
-      {/* Grid */}
-      <div className="pointer-events-none absolute inset-0 grid-bg" />
-
-      {/* Text */}
-      <h1 className="relative z-10 text-white uppercase text-[clamp(4rem,12vw,10rem)] font-semibold tracking-tight">
-        owled
-      </h1>
-
-    </main>
-    <div>
-      <Temp />
       <div>
-        <Temp />
+        <Animation />
+        <div className="h-screen bg-black">
+
+        </div>
       </div>
-    </div>
+
     </section>
   );
 }
+
+
+    // <main
+    //   ref={heroRef}
+    //   onMouseMove={handleMouseMove}
+    //   className="relative flex h-screen w-screen items-center justify-center bg-black"
+    // >
+    //   {/* Grid */}
+    //   <div className="pointer-events-none absolute inset-0 grid-bg" />
+
+    //   {/* Text */}
+    //   <h1 className="relative z-10 text-white uppercase text-[clamp(4rem,12vw,10rem)] font-semibold tracking-tight">
+    //     owled
+    //   </h1>
+
+    // </main>
+    // <div>
+    //   <Temp />
+    //   <div>
+    //     <Temp />
+    //   </div>
+    // </div>
